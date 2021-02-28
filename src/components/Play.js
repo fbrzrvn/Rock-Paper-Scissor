c6250a3e98f2358a39c8b0a3dea25537352a1227
fabrizio <faber.rvn@gmail.com>
2021-02-28 13:09:19 +0100

    #3 Play Component

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Triangle from '../images/bg-triangle.svg';
 
-const Play = () => {
+const Play = ({ setUserChoice}) => {
+
+  const handleClick = e => {
+    setUserChoice(e.target.dataset.id);
+  }
+
   return (
-    <div>
-    Play
+    <div className="play">
+      <img src={Triangle} alt="traingle-bg" className="triangle" />
+      <div className="items">
+        <Link to="/game">
+          <div
+            data-id="rock"
+            className="icon icon--rock"
+            onClick={handleClick}
+          ></div>
+        </Link>
+      </div>
+      <div className="items">
+        <Link to="/game">
+          <div
+            data-id="paper"
+            className="icon icon--paper"
+            onClick={handleClick}
+          ></div>
+        </Link>
+      </div>
+      <div className="items">
+        <Link to="/game">
+          <div
+            data-id="scissors"
+            className="icon icon--scissors"
+            onClick={handleClick}
+          ></div>
+        </Link>
+      </div>
     </div>
   )
 }
