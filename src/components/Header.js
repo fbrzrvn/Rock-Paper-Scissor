18b8730517dd9eb98b5d4eb6cd76b14ec0bfdfb3
fabrizio <faber.rvn@gmail.com>
2021-02-28 16:47:46 +0100

    #5 Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Header = () => {
+const Header = ({ score }) => {
   return (
-    <div>
-      Header
+    <div className="header">
+      <div className="text">
+        <span>Rock</span>
+        <span>Paper</span>
+        <span>Scissors</span>
+      </div>
+      <div className="score-box">
+        <span>Score</span>
+        <div className="score-box__score">
+          <p>{score}</p>
+        </div>
+      </div>
     </div>
   )
 }
