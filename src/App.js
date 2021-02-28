d817d488924b351d1961a85a1b7f28eb9cef8a31
fabrizio <faber.rvn@gmail.com>
2021-02-28 12:12:42 +0100

    #1 Set Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Play from './components/Play';
+import Game from './components/Game';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Play />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/game" exact>
+            <Game />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </>
   );
 }
 
